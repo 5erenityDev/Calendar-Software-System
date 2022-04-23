112c15bd24b918a1e9257175be89c31990dd286a
Sean Blankenship <seanblankenship2002@gmail.com>
2022-04-23 17:19:23 -0400

    Added the basic calendar system
    This serves as a loose outline to place all of our code into.

---
+++ CalendarSoftwareSystem/EventForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalendarSoftwareSystem
+{
+    public partial class EventForm : Form
+    {
+        public EventForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EventForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEveSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
